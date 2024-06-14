98f8420374cf4d00ab5c0d0028653b8d899e97ac
gyeolse <threewave@kakao.com>
2024-06-14 15:49:33 +0900

    [ADD] add linking notion

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718341378',resources: [
+    cacheName: 'chirpy-1718362476',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
