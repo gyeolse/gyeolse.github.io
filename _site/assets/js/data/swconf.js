0c6a7b5e093911890e5c508b253ba34ad313480d
gyeolse <threewave@kakao.com>
2024-06-14 13:30:05 +0900

    [BUG] Fix CI/CD #2

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718337859',resources: [
+    cacheName: 'chirpy-1718339380',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
