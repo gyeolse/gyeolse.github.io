5c5efcc66e55ed14fe6a0d3baf053dd4ea562067
gyeolse <segylee5030@gmail.com>
2024-06-17 23:12:42 +0900

    [ENH] : Modify jekyll.yml

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718362476',resources: [
+    cacheName: 'chirpy-1718633506',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
